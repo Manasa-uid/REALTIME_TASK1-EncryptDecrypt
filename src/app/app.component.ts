2cfe3153fc3aeea444b3e4ca68e8a5ea935a3df3
Manasa-uid <mansinayak10@gmail.com>
2020-01-18 16:15:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Posts } from './post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'crud-Imr';
+export class AppComponent implements OnInit {
+  public id:number;
+  public userId:number;
+  public title:string;
+  public body:string;
+  constructor(){}
+
+   public postInfo:any[] = [];
+  ngOnInit(){}
+
+       create(){
+         let postData:Posts = {
+              id:this.id,
+              userId:this.userId,
+              title:this.title,
+              body:this.body
+         }
+
+             this.api.postInfo(postData).subscribe((data)=>{
+               this.postInfo.push(data);
+             })
+       }
+   
 }
